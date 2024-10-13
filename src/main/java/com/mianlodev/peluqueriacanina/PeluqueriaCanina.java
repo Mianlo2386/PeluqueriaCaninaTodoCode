5bd51ea793b36ed87459dc038257a95650f51e1b
Miguel Lopez <miguellopez32@gmail.com>
2024-10-13 12:23:01 -0300

    IGU de cargar datos completado

---
+++ src/main/java/com/mianlodev/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,15 @@
+package com.mianlodev.peluqueriacanina;
+
+import com.mianlodev.peluqueriacanina.igu.Principal;
+
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+        
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+        
+        
+    }
+}
